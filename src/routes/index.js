5cbcb9b438151984115816e0be222312974d3f88
Nguyen Minh Hien <minhhien30201@gmail.com>
2025-02-11 22:59:02 +0700

    structure

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,7 @@
-const express = require('express');
-const router = express.Router();
+const siteRouter = require('./siteRouter');
 
-router.get('/', (req, res) => {
-    res.send('Trang chủ');
-});
+function routes(app) {
+    app.use('/', siteRouter);
+}
 
-module.exports = router; // ✅ Export đúng cách
+module.exports = routes;
