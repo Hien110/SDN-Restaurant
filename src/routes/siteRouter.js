62886244a5c0a0db83de69975e5d4074a4d4fd7b
Nguyen Minh Hien <minhhien30201@gmail.com>
2025-02-25 21:30:04 +0700

    UI Login

--- src/routes/siteRouter.js
+++ src/routes/siteRouter.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const siteController = require('../app/controllers/SiteController');
 
-router.get('/', siteController.index);
+router.get('/login', siteController.index);
+router.get('/register', siteController.register);
+
 
 module.exports = router;
