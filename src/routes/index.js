8b8fc22cbbc1932ef2c314ef9c326e016cb05ea8
Nguyen Minh Hien <minhhien30201@gmail.com>
2025-03-01 21:19:22 +0700

    update UI login

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,10 +8,11 @@ const bookingRouter = require("./bookingTableRouter");
 const restaurantRouter = require("./restaurantsRouter");
 const { getFooterData } = require("../app/controllers/RestaurantsController");
 const userRoutes = require("./usersRoutes");
+const isAuth = require("../app/middlewares/is-auth");
 
 function routes(app) {
   app.use("/restaurantInfor", restaurantRouter);
-  app.use("/", getFooterData, siteRouter);
+  app.use("/",isAuth.setUser, getFooterData, siteRouter);
   app.use("/", authRouter);
   app.use('/users', userRoutes); 
   app.use('/bookingTable', bookingRouter); 
